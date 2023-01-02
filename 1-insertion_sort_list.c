ae80ec5cd1d5a0b2eb3a2e8ad5186663d466c516
AlexandreGRN <tulkiidra@gmail.com>
2023-01-02 07:32:48 -0800

    Betty coding style + remove useless files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,49 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - Sort linked list using insertion type of sorting
+ * @list: linked list
+ */
 void insertion_sort_list(listint_t **list)
 {
-    int test = 0; /* test if sorted */
-    listint_t *copyNode1;
-    listint_t *copyNode2;
-    listint_t *head = *list;
+	int test = 0; /* test if sorted */
+	listint_t *copyNode1, *copyNode2;
+	listint_t *head = *list;
 
-    while (test == 0)
-    {
-        test = 1;
-        head = *list;
-        while (head)
-        {
-            if ((head)->next && (head)->n > (head)->next->n)
-            {
+	if (!(*list))
+		return;
+	while (test == 0)
+	{
+		test = 1;
+		head = *list;
+		while (head)
+		{
+			if ((head)->next && (head)->n > (head)->next->n)
+			{
+				copyNode1 = (head)->next;
+				copyNode2 = (head)->prev;
 
-                copyNode1 = (head)->next;
-                copyNode2 = (head)->prev;
+				if ((head)->next->next)
+					(head)->next->next->prev = (head);
+				(head)->next = (head)->next->next;
 
-                if ((head)->next->next)
-                    (head)->next->next->prev = (head);
-                (head)->next = (head)->next->next;
-                
-                if (copyNode1 != NULL)
-                    (copyNode1)->next = (head);
-                (head)->prev = (copyNode1);
+				if (copyNode1 != NULL)
+					(copyNode1)->next = (head);
+				(head)->prev = (copyNode1);
 
-                if (copyNode1 != NULL)
-                    (copyNode1)->prev = copyNode2;
-                if (copyNode2 != NULL)
-                    (copyNode2)->next = copyNode1;
-                else /* if copyNode2 = NULL means copyNode1 is the new head*/
-                    *list = copyNode1;
+				if (copyNode1 != NULL)
+					(copyNode1)->prev = copyNode2;
+				if (copyNode2 != NULL)
+					(copyNode2)->next = copyNode1;
+				else /* if copyNode2 = NULL means copyNode1 is the new head*/
+					*list = copyNode1;
 
-
-
-                test = 0;
-                print_list(*list);
-            }
-            head = (head)->next;
-            if (test == 0)
-                break;
-        }
-    }
+				test = 0;
+				print_list(*list);
+			}
+			head = (head)->next;
+			if (test == 0)
+				break;
+		}
+	}
 }
